d931682638095652e1608ce9fba68810438b086a
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-08-13 21:22:41 -0700

    add galleries

--- src/app/galleries/page.tsx
+++ src/app/galleries/page.tsx
@@ -1,42 +1,24 @@
 import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
-export default function Galleries() {
+export default function Galleries(albums) {
   return (
-
-  )
+      <section className="gallery-display">
+          <div className="">
+              {albums.map((album) => (
+                  <div key={album.id} className="album-item">
+                      <img
+                          src={`https://live.staticflickr.com/${album.server}/${album.primary}_${album.secret}_c.jpg`}
+                          alt={album.title._content}
+                      />
+                      <Link href={`/galleries/${album.id}`}>
+                          <h4 className="album-title">
+                              {album.title._content}
+                          </h4>
+                      </Link>
+                  </div>
+              ))}
+          </div>
+      </section>
+  );
 }
-
-
-
-import React, { useState, useEffect } from "react";
-import { Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import "./Galleries.css";
-import useFetchFlickr from "../../hooks/useFetchFlickr";
-
-const Galleries = () => {
- const { data: albums, loading } = useFetchFlickr("/flickr/albums");
-
-
-    return (
-        <Row className="g-0 gallery-display">
-            <div className="album-grid">
-                {albums.map((album) => (
-                    <div key={album.id} className="album-item">
-                        <img
-                            src={`https://live.staticflickr.com/${album.server}/${album.primary}_${album.secret}_c.jpg`}
-                            alt={album.title._content}
-                        />
-                        <Link to={`/galleries/${album.id}`}>
-                            <h4 className="album-title">
-                                {album.title._content}
-                            </h4>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </Row>
-    );
-};
-
-export default Galleries;
